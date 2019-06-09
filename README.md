eb94186aac5f7308afd6111a19cb858bdec933fe
David Hwang <d.hw4ng@gmail.com>
2019-06-08 22:59:24 -0700

    replace tslint with eslint

---
+++ README.md
@@ -0,0 +1,21 @@
+# Foods
+
+I'm trying to learn Node.js by implementing a simple CRUD API.
+
+I'm doing a recipe list.
+
+Node.js 10 supports ES2018 features.
+
+## Data Models
+
+* Recipe
+* Ingredients
+* Attachments (screenshots, etc.)
+
+## Common Tasks
+
+Dev work is done with
+
+```sh
+yarn run watch
+```
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
-import express from "express"
+import express from "express";
+import router from "./routes/index";
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!")
-})
+app.use("/", router);
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, (): void => {
+  console.log(`Example app listening on port ${port}!`);
+});
