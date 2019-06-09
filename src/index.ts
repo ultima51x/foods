191ae34290fff88b6c07417d750545e99de81ef8
David Hwang <d.hw4ng@gmail.com>
2019-06-09 01:32:48 -0700

    eslint/prettier spacing inconsistencies

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
-import express from 'express';
-import morgan from 'morgan';
-import router from './routes/index';
+import express from "express";
+import morgan from "morgan";
+import router from "./routes/index";
 
 const app = express();
 const port = 3000;
 
 // http request logging
-app.use(morgan('combined'));
+app.use(morgan("combined"));
 
-app.use('/', router);
+app.use("/", router);
 
 app.listen(port, (): void => {
   console.log(`Starting foods ${port}!`);
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,12 @@
-import express from 'express';
-import recipes from './recipes';
+import express from "express";
+import recipes from "./recipes";
 
 const router = express.Router();
 
-router.use('/recipes', recipes);
+router.use("/recipes", recipes);
 
-router.get('/', (req, res): void => {
-  res.send('This is the root!');
+router.get("/", (req, res): void => {
+  res.send("This is the root!");
 });
 
 export default router;
